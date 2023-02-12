a30ca8da1fd94901ecc7548a623648be3a4b8719
kethnal <kaseyvaldez@hotmail.com>
2023-02-12 05:35:19 -0800

    Make profile header

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,19 +1,23 @@
-import Image from "next/image"
+import '../../../styles/Profile.scss';
 import PocketBase from 'pocketbase';
+import Image from "next/image";
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 export default async function Profile({ params }: any) {
   const user = await pb.collection('users').getOne(params.id);
-  const resultList = await pb.collection('restaurants').getList(1, 50, {
+  const userRestaurants = await pb.collection('restaurants').getList(1, 50, {
     filter: `user_id = "${params.id}"`,
 });
 
   return (
     <div className='Profile'>
-      <div>
       <Image src={user.avatar} alt='avatar' width={120} height={120}/>
-      <h1>{user.username}</h1>
+      <div className='section-right'>
+        <h2>@{user.username}</h2>
+        <button className='btn'>
+          Share Profile
+        </button>
       </div>
     </div>
   )
