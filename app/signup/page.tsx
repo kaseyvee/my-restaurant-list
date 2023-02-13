fd66b2b8a31e79323ac33812c7d5825e2e5df90e
kethnal <kaseyvaldez@hotmail.com>
2023-02-13 11:53:48 -0800

    Make loading component

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,17 @@
+'use client';
+
 import '../../styles/LogInSignUp.scss';
 import Image from 'next/image';
 import HomeForm from '../../components/HomeForm';
 import PocketBase from 'pocketbase';
+import ReactLoading from 'react-loading';
+import { useState } from 'react';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
-// example create data
-
 export default function SignUp() {
+  const [loading, setLoading] = useState(false);
+
   async function handleSignUp() {
     const data = {
         "username": "test_username",
@@ -18,15 +22,26 @@ export default function SignUp() {
         "avatar": "https://example.com"
     };
     const record = await pb.collection('users').create(data);
-  }
+  };
+
+  const loadingStyle = {
+    justifyContent: 'center',
+  };
+  
   return (
-    <div className='Form'>
-      <div className='title'>
-        <h1>Sign Up</h1>
-        <a href='/'><Image src='/../public/back.png' alt='back' width={58} height={58}/></a>
-      </div>
-      <HomeForm signup={true} />
-      <button className='btn'>Sign Up</button>
+    <div className='Form' style={loading ? loadingStyle : {}}>
+     {loading ? 
+        <ReactLoading type={"spinningBubbles"} color={"#ffffff"} width={80} />
+      :
+        <>
+          <div className='title'>
+            <h1>Sign Up</h1>
+            <a href='/'><Image src='/../public/back.png' alt='back' width={58} height={58}/></a>
+          </div>
+          <HomeForm signup={true} />
+          <button className='btn'>Sign Up</button>
+        </>
+      }
     </div>
   )
 }
